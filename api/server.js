5bdf559879c04c6cb53b7f8d6c8e0540a3a83e86
Shane Jeremich <shane.jeremich@gmail.com>
2020-01-24 12:47:22 -0700

    added more folders

--- api/server.js
+++ api/server.js
@@ -1,14 +1,24 @@
 const express = require("express");
 const helmet = require("helmet");
 
-const ProjectsRouter = require("../projects/projects.router");
+// const ProjectsRouter = require("./projects/router");
+// const ResourcesRouter = require("./resources/router");
+// const TasksRouter = require("./tasks/router");
 
 const server = express();
 server.use(express.json());
 server.use(helmet());
 server.use(logger);
 
-server.use("/api/projects", ProjectsRouter);
+// server.use("/api/projects", ProjectsRouter);
+// server.use("/api/resources", ResourcesRouter);
+// server.use("/api/tasks", TasksRouter);
+
+server.get("/", (req, res) => {
+  res.send({
+    Server: "Hi, I'm your Server.  Can I start you off with a drink?"
+  });
+});
 
 function logger(req, res, next) {
   const { method, url } = req;
--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const server = require("./api/server");
 const PORT = process.env.PORT || 4000;
 
 server.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
+  console.log(`\n*** Server Running on: http://localhost:${PORT} ***\n`);
 });
